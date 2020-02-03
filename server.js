f16d5c94d5b0f50bd4a5d510cc2e5fe21e00a280
heliumface770 <heliumface770@gmail.com>
2020-02-02 22:17:01 -0700

    server.js added listener, middleware, and routes

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const Example = require("./exampleModel.js");
+const port = process.env.port || 3000;
+const app = express();
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true });
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{ useNewUrlParser: true, useFindAndModify: false });
+
+app.use(require('./routes/APIroutes'));
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+})
