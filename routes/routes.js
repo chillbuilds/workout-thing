4311e0433085e39f73690f1e24ab10a99137ac21
heliumface770 <heliumface770@gmail.com>
2020-02-02 23:26:20 -0700

    C:/Program Files/Git/api/workouts/range route added, stats page functioning

--- routes/routes.js
+++ routes/routes.js
@@ -13,11 +13,17 @@ router.get('/stats', function(req, res){
     res.sendFile(path.join(__dirname, "../public/stats.html"))
 })
 
-router.get("/api/workouts", (req, res) => {
+router.get('/api/workouts', (req, res) => {
+    Workout.find({})
+    .then(workout => {
+        res.json(workout)
+    }).catch(err => {res.status(404).json(err)})
+})
+router.get('/api/workouts/range', (req, res) => {
     Workout.find({}).sort({ date: -1 })
     .then(workout => {
         res.json(workout)
-    }).catch(err => {res.status(400).json(err)})
+    }).catch(err => {res.status(404).json(err)})
 })
 
 module.exports = router;
